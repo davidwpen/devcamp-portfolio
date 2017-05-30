91ca1887a8f5fc05f4e5d19caee535d6a200fa6f
David Penny <davidpenny@Mac-mini-de-Pablo.local>
2017-05-30 13:11:14 +0200

    integrated index action portafolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Blog.create(title: 'post by seed', body:'content by seed')
+10.times do |blog|
+	Blog.create!(
+		title: "my blog post #{blog}",
+	)
+end
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "portfolio title: #{portfolio_item}" ,
+		subtitle: "my great service",
+		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui perspiciatis fugit error numquam impedit corporis nulla quam deserunt consectetur, iure, dolore soluta voluptate sit at ducimus id. Quis, dolor, dolores?",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+	)
+end
